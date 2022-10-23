dedc5a131044bdee78733dea02609cd6836ebe1b
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-10-22 20:24:40 -0500

    Created contract to test the flashloan feature from the AAVE Protocol - Added all the required interfaces

---
+++ AAVE/contracts/AaveFlashLoan.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./interfaces/FlashLoanReceiverBase.sol";
+
+contract AaveFlashLoan is FlashLoanReceiverBase {
+  using SafeMath for uint;
+
+  event Log(string message, uint val);
+
+  constructor(ILendingPoolAddressesProvider _addressProvider) public FlashLoanReceiverBase(_addressProvider) {}
+
+  function testFlashLoan(address asset, uint amount) external {
+    uint bal = IERC20(asset).balanceOf(address(this));
+    require(bal > amount, "bal <= amount");
+
+    address receiver = address(this);
+
+    address[] memory assets = new address[](1);
+    assets[0] = asset;
+
+    uint[] memory amounts = new uint[](1);
+    amounts[0] = amount;
+
+    // 0 = no debt, 1 = stable, 2 = variable
+    // 0 = pay all loaned
+    uint[] memory modes = new uint[](1);
+    modes[0] = 0;
+
+    address onBehalfOf = address(this);
+
+    bytes memory params = ""; // extra data to pass abi.encode(...)
+    uint16 referralCode = 0;
+
+    LENDING_POOL.flashLoan(
+      receiver,
+      assets,
+      amounts,
+      modes,
+      onBehalfOf,
+      params,
+      referralCode
+    );
+  }
+
+  /**
+   * @dev executeOperation() function is called by Aave as part of the execution of the flashLoan() function
+   * @dev when this function is called, the borrowed tokens are available to be spent within the execution of the function, and at the end of the execution the borrowed tokens must be repayed
+   * @param assets -> The assets that were requested
+   * @param amounts -> The amounts that were borrowed
+   * @param premiums -> The fees to pay for the borrow
+   * @param initiator -> The address that triggers the flashloan
+   * @param params -> Any other extra data (Encoded)
+   */
+  function executeOperation(
+    address[] calldata assets,
+    uint[] calldata amounts,
+    uint[] calldata premiums,
+    address initiator,
+    bytes calldata params
+  ) external override returns (bool) {
+    // do stuff here (arbitrage, liquidation, etc...)
+    // abi.decode(params) to decode params
+    for (uint i = 0; i < assets.length; i++) {
+      emit Log("borrowed", amounts[i]);
+      emit Log("fee", premiums[i]);
+
+      uint amountOwing = amounts[i].add(premiums[i]);
+      IERC20(assets[i]).approve(address(LENDING_POOL), amountOwing);
+    }
+    // repay Aave
+    return true;
+  }
+
+
+}
